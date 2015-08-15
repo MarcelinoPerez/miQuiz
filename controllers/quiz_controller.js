24b2b7eba497b51060d2f61a1ec558551c6179ee
Marcelino Perez <aticomunication@gmail.com>
2015-08-15 01:59:00 +0100

    Añadir un buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,21 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
+
+  console.log("contenido: " + req.query.search)
+  if(req.query.search == undefined){
+    search = '%';
+  }
+  else{
+    search = '%' + req.query.search + '%';
+  }
+  
+ models.Quiz.findAll({where: ["pregunta like ?", search  ]}).then(
     function(quizes) {
       res.render('quizes/index', { quizes: quizes});
     }
   ).catch(function(error) { next(error);})
+
 };
 
 // GET /quizes/question
